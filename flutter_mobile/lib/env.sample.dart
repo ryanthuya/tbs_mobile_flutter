a13e6a31fd7c0482b52bec44f60415cb13a393ed
Min Thu Ya Kyaw <minthuyakyaw@gmail.com>
2022-09-01 09:51:08 +0630

    Added API URL  in .env

--- flutter_mobile/lib/env.sample.dart
+++ flutter_mobile/lib/env.sample.dart
@@ -1,4 +1,5 @@
 class Env {
   //  Student API URL FROM BACKEND LARAVEL
-  static String URL_PREFIX = "http://127.0.0.1:8000/api/students/";
+  // static String URL_PREFIX = "http://127.0.0.1:8000/api/students/";
+static String URL_PREFIX = "http://192.168.99.79:6969/students";
 }
