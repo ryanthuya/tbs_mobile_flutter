3cdff548e76d42fc8316cb10374ab5312acc4cf6
Min Thu Ya Kyaw <minthuyakyaw@gmail.com>
2022-09-01 21:46:44 +0630

    Chanage API URL in  Global env

--- flutter_mobile/lib/env.sample.dart
+++ flutter_mobile/lib/env.sample.dart
@@ -2,5 +2,5 @@ class Env {
   //  Student API URL FROM BACKEND LARAVEL
 
   // static String URL_PREFIX = "http://127.0.0.1:8000/api/students/";
- static String URL_PREFIX = "http://192.168.100.11:6969/api/students/";
+ static String URL_PREFIX = "http://192.168.99.79:6969/api/students/";
 }
